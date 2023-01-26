d365730e94a6f6d96392788ac0665999826af75e
Manikanta-44 <93340062+Manikanta-44@users.noreply.github.com>
2023-01-26 21:17:50 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet/less" href="css/style.less">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/aos/2.3.4/aos.css" integrity="sha512-1cK78a1o+ht2JcaW6g8OXYwqpev9+6GqOkz9xmBN9iUUhIndKtxwILGWYOSibOKjLsEdjyjZvYDq/cZwNeak0w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-white custom-nav">
+  <div class="container">
+    <a class="navbar-brand active" href="https://drguravareddy.com/">
+ <img src="images/website-logo.png" alt="" width="100%"> </a>
+    <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+ </button> 
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+        <li class="nav-item pe-3 ps-3">
+
+          <a class="nav-link" href="/professional">Services</a>
+        </li>
+        <li class="nav-item active pe-3 ps-3">
+          <a class="nav-link" aria-current="page" href="/social">Product</a>
+        </li>
+        <li class="nav-item pe-3 ps-3">
+          <a class="nav-link" href="/philanthropy.php">Technology</a>
+        </li>
+        <li class="nav-item pe-3 ps-3">
+          <a class="nav-link" href="/awards-recognition">About</a>
+        </li>
+        <li class="nav-item pe-3 ps-3">
+          <a class="nav-link" href="/gallery">Client</a>
+        </li>
+        <li class="nav-item pe-3 ps-3">
+          <a class="nav-link" href="/gallery">Partner
+</a>
+        </li>
+
+
+      </ul>
+        <a href="https://www.facebook.com/drguravareddy" target="_blank"><img src="images/home-image.png" class="pe-2 ps-2" alt=""></a>                
+
+            <a href="https://www.instagram.com/guravareddy/" target="_blank"><img src="images/mail.png" class="pe-2 ps-2" alt=""></a>
+            <a href="https://www.youtube.com/c/DrGuravaReddy" target="_blank"><img src="images/shuffle.png" class="pe-2 ps-2" alt=""></a>
+    </div>
+  </div>
+</nav>
