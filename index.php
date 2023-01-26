3e68c8fadcdabb1097a2689ad02e6d2b4773b688
Manikanta-44 <93340062+Manikanta-44@users.noreply.github.com>
2023-01-26 21:13:33 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+$title = "Hetero Website";
+$metadesc = "";
+$keywords = "";
+include 'includes/header.php';?>
+<section class="hero-section">
+    <div class="container">
+        <div class="row column-reverse">
+        <div class="col-md-6" data-aos="fade-up"
+     data-aos-easing="linear"
+     data-aos-duration="1500">
+            <h1 class="fw-bold pb-5">Making the most of the <br class="d-none d-lg-block"> ever-growing <br class="d-none d-lg-block">
+<span class="spanred-color"> Information Technology</span></h1>
+<h5 class="pb-4">Managed by a team of professional experts with extensive <br> experience and impressive track records</h5>
+<a href="" class="text-decoration-none"><button class="readmore-btn">Read More</button></a>
+        </div>
+        <div class="col-md-6" data-aos="fade-down"
+     data-aos-easing="linear"
+     data-aos-duration="1500">
+        <img src="images/homepage-hero-image.png" alt="">
+        </div>
+        </div>
+    </div>
+</section>
+<section class="welcome-dsi">
+    <div class="container">
+        <div class="row align-items-center">
+            <div class="col-md-12 col-lg-6">
+              <img src="images/dsi-image1.png" width="100%" alt="">
+            </div>
+            <div class="col-md-12 col-lg-6">
+            <h2 class="pb-4">Welcome to <span class="spanred-color">Dwidasa Samsara <br class="d-none d-lg-block">
+             Indonesia (DSI)</span> </h2>
+             <p class="pb-4">Dwidasa Samsara Indonesia (DSI) was firstly established in 2010 by the founders, who each of them has a common end objective to innovate new creations by making the most of the ever-growing information technology through DSI’s distinct front-end based application concept.
+</p>
+<p>Managed by a team of professional experts with extensive experience and impressive track records, DSI believes that continuous improvements and innovations assure your business to run effectively and efficiently.</p>
+            </div>
+</div>
+    </div>
+</section>
+<section class="product-and-service mt-5 mb-5 aos-hidden">
+    <div class="container">
+        <h2 class="text-center pb-4 fw-bold">Product and Service</h2>
+        <div class="row justify-content-center">
+            <div class="col-md-6 col-lg-4" data-aos="fade-right">
+                <img src="images/product-image1.png" alt="">
+                <h3 class="span-redcolor pt-3 pb-3">Our Product</h3>
+                <p>Our product is made on the base of our <br class="d-none d-md-block"> team’s careful research and analyses, <br class="d-none d-md-block"> ranging from internet based <br class="d-none d-md-block"> application.</p>
+                <a href="" class="text-decoration-none"><button class="readmore-btn">Read More</button></a>
+            </div>
+            <div class="col-md-6 col-lg-4">
+                <img src="images/serviceverified-logo.png" alt="">
+                <h3 class="span-redcolor pt-3 pb-3">Our Service</h3>
+                <p>DSI’s shared service solutions focus on <br class="d-none d-md-block"> the front-end based software <br class="d-none d-md-block"> development, designed specifically for <br class="d-none d-md-block"> the banking and financial sectors.
+</p>
+                <a href="" class="text-decoration-none"><button class="readmore-btn">Read More</button></a>
+            </div>
+            <div class="col-md-6 col-lg-4" data-aos="fade-left">
+            <img src="images/technology-image1.png" alt="">
+            <h3 class="span-redcolor pt-3 pb-3">Our Technology</h3>
+                <p>First JAVA, runs on more than 850 <br class="d-none d-md-block"> million personal computers worldwide, <br class="d-none d-md-block"> and on billions of devices worldwide, <br class="d-none d-md-block"> including mobile and TV devices.</p>
+                <a href="" class="text-decoration-none"><button class="readmore-btn">Read More</button></a>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+$title = "Hetero Website";
+$metadesc = "";
+$keywords = "";
+include 'includes/footer.php';?>
